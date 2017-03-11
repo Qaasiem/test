c7c88d6ac931a1c8ea0642ed6b732f03d94871f1
qaasiem samaai <qaasiemsamaai@gmail.com>
2017-03-11 22:00:39 +0200

    second commit

---
+++ Activity1/src/main/java/com/pr1/App.java
@@ -0,0 +1,14 @@
+package com.pr1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println( " Understanding Github and Maven " );
+    }
+}
